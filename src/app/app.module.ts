9d81123214fa85dc3e6820c782690ff21282bf7d
rwedzony <rwedzony@gmail.com>
2020-08-14 11:01:23 +0200

    adding functionality to login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatCardModule} from "@angular/material/card";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
 
 
 
@@ -36,7 +37,8 @@ import {MatButtonModule} from "@angular/material/button";
     MatIconModule,
     MatCardModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule
   ],
     providers: [],
     exports: [
--- src/app/authentification/login/login.component.ts
+++ src/app/authentification/login/login.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  login: string = '';
+  password: string = '';
   constructor() { }
   hide=true;
   ngOnInit(): void {
   }
 
+
+  SignIn() {
+    let login1=this.login;
+    let pass1=this.password;
+    this.login='';
+    this.password='';
+    console.log('login:' + login1);
+    console.log('password:' + pass1);
+
+  }
 }
