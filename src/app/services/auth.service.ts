c9ad8d20664bce584b811b8984772ab07c62a886
rwedzony <rwedzony@gmail.com>
2020-08-31 14:56:32 +0200

    Added Auth service for User Authentication

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {filter, map} from "rxjs/operators";
+import {Employee} from "../datamodels/employee";
+import {LoginRequestPayload} from "../datamodels/login-request.payload";
+import {LoginResponsePayload} from "../datamodels/login-response.payload";
+import { LocalStorageService } from 'ngx-webstorage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+ // USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
+ // URL_EMPLOYEES = 'http://localhost:8080/employees'
+  loginURL = 'http://localhost:8080/auth/login';
+
+
+  constructor(private httpClient: HttpClient,
+              private localStorage: LocalStorageService) {
+  }
+
+  login(loginRequestPayload: LoginRequestPayload) {
+
+    return this.httpClient.post<LoginResponsePayload>(this.loginURL,loginRequestPayload)
+      .pipe(map(data => {
+        this.localStorage.store('token', data.authToken);
+        this.localStorage.store('firstName', data.firstName);
+        this.localStorage.store('lastName', data.lastName);
+        this.localStorage.store('role', data.role);
+      }));
+  }
+  getJwtToken(){
+    return this.localStorage.retrieve('token');
+  }
+  getUserFirstName(){
+    return this.localStorage.retrieve('firstName');
+  }
+  getUserLastName(){
+    return this.localStorage.retrieve('lastName');
+  }
+  getUserRole(){
+    return this.localStorage.retrieve('role');
+  }
+  logout() {
+    this.localStorage.clear('token');
+    this.localStorage.clear('firstName');
+    this.localStorage.clear('lastName');
+    this.localStorage.clear('role');
+  }
+}
