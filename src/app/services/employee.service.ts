41f62538fc5f2709dd843a31c060510de593bfc3
rwedzony <rwedzony@gmail.com>
2021-09-19 18:34:18 +0200

    employee service refactor

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,11 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
-import { Employee } from '../datamodels/employee';
-import {AuthService} from "./auth.service";
+import {Employee} from '../datamodels/employee';
 import {BaseurlService} from "./baseurl.service";
 import {Employeeuser} from "../datamodels/employeeuser";
-import {ToastrService} from "ngx-toastr";
 
 @Injectable({
   providedIn: 'root'
@@ -14,68 +11,54 @@ export class EmployeeService {
 
   urlGet: string;
 
-   httpOptions = {
+  httpOptions = {
     headers: new HttpHeaders({
       'Access-Control-Allow-Origin': '*'
-    })};
+    })
+  };
 
-  constructor(private httpClient:HttpClient,
+  constructor(private httpClient: HttpClient,
               private baseurl: BaseurlService) {
-    this.urlGet=baseurl.getBaseUrl()+'/employees'
+    this.urlGet = baseurl.getBaseUrl() + '/employees';
   }
 
-  public getEmployees() {
+  public getEmployees(): any {
     return this.httpClient.get(this.urlGet);
   }
 
-  public getAllEmployeesCount() {
-    let url=this.urlGet+'/'+'count';
+  public getAllEmployeesCount(): any {
+    const url = this.urlGet + '/' + 'count';
     return this.httpClient.get(url);
   }
 
-  public getAllEmployeesWagesCount() {
-    let url=this.urlGet+'/'+'wages';
+  public getAllEmployeesWagesCount(): any {
+    const url = this.urlGet + '/' + 'wages';
     return this.httpClient.get(url);
   }
 
-  public deleteEmployeesById(id: number){
-    let urlDelete=this.urlGet+'/'+id.toString();
-    // this.httpClient.delete(urlDelete,this.httpOptions).subscribe(
-    //   (value) => {console.log('Received value: ',value)},
-    //   (error) => {console.log('Error!!',error)},
-    //   ()=>{console.log('end of values')}
-    // );
-
+  public deleteEmployeesById(id: number): any {
+    const urlDelete = this.urlGet + '/' + id.toString();
     return this.httpClient.delete(urlDelete);
   }
 
-  addEmployee(employee: Employee) {
-    // this.httpClient.post(this.urlGet,employee).subscribe(
-    //   (value) => {console.log('Received value: ',value)},
-    //   (error) => {console.log('Error!!',error)},
-    //   ()=>{console.log('end of values')});
-    return this.httpClient.post(this.urlGet,employee);
+  addEmployee(employee: Employee): any {
+    return this.httpClient.post(this.urlGet, employee);
   }
 
-  updateEmployee(employee: Employee) {
-    let urlPut = this.urlGet + '/' + employee.id.toString();
-
-    // this.httpClient.put(urlPut,employee).subscribe(
-    //   (value) => {console.log('Connection okey')},
-    //   (error) => {console.log('Something went wrong')}
-    //   );
-
+  updateEmployee(employee: Employee): any {
+    const urlPut = this.urlGet + '/' + employee.id.toString();
     return this.httpClient.put(urlPut, employee);
-  };
+  }
 
-
-  updatePatchEmployee(employeeuser: Employeeuser ) {
-    let urlPatch=this.urlGet+'/'+employeeuser.id.toString();
-    this.httpClient.patch(urlPatch,employeeuser,this.httpOptions).subscribe(
-      (value) => {console.log('Connection okey')
+  updatePatchEmployee(employeeuser: Employeeuser): any {
+    const urlPatch = this.urlGet + '/' + employeeuser.id.toString();
+    this.httpClient.patch(urlPatch, employeeuser, this.httpOptions).subscribe(
+      (value) => {
+        console.log('Connection okey')
       },
-      (error) => {console.log('Something went wrong')},
+      (error) => {
+        console.log('Something went wrong')
+      },
     );
   }
-
 }
