eed87e336584dcaec5c5543b6e0792cdbe8b0700
rwedzony <rwedzony@gmail.com>
2021-09-21 16:53:36 +0200

    addtaskdialog component refactor

--- src/app/content/admin/addtaskdialog/addtaskdialog.component.ts
+++ src/app/content/admin/addtaskdialog/addtaskdialog.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatDialogRef} from "@angular/material/dialog";
-import {EmployeeService} from "../../../services/employee.service";
-import {TaskService} from "../../../services/task.service";
-import {Employee} from "../../../datamodels/employee";
-import {Task} from "../../../datamodels/task";
-import {MatDatepicker} from "@angular/material/datepicker";
+import {MatDialogRef} from '@angular/material/dialog';
+import {EmployeeService} from '../../../services/employee.service';
+import {TaskService} from '../../../services/task.service';
+import {Employee} from '../../../datamodels/employee';
+import {Task} from '../../../datamodels/task';
+import {MatDatepicker} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-addtaskdialog',
@@ -14,42 +14,40 @@ import {MatDatepicker} from "@angular/material/datepicker";
 export class AddtaskdialogComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<AddtaskdialogComponent>,
-              private taskService: TaskService) { }
+              private taskService: TaskService) {
+  }
 
-
-  task:Task;
-  taskStartDate:Date;
-  taskEndDate:Date;
-
+  task: Task;
+  taskStartDate: Date;
+  taskEndDate: Date;
 
   ngOnInit(): void {
-    this.taskStartDate=new Date();
-    this.taskEndDate=new Date();
-    console.log('startdate: '+this.taskStartDate);
+    this.taskStartDate = new Date();
+    this.taskEndDate = new Date();
+    console.log('startdate: ' + this.taskStartDate);
 
-    this.task=new Task();
+    this.task = new Task();
     this.task.id = 0;
-    this.task.description ='';
+    this.task.description = '';
     this.task.status = 'NEW';
     this.task.startDate = '';
-    this.task.endDate ='';
+    this.task.endDate = '';
 
   }
 
-
-  onNoClick() {
+  onNoClick(): void {
     this.dialogRef.close();
   }
 
   confirmAdd(): void {
-    this.task.startDate=this.taskStartDate.toLocaleDateString();
-    this.task.endDate=this.taskEndDate.toLocaleDateString();
-    this.taskService.addTask(this.task)
+    this.task.startDate = this.taskStartDate.toLocaleDateString();
+    this.task.endDate = this.taskEndDate.toLocaleDateString();
+    this.taskService.addTask(this.task);
     this.dialogRef.close();
 
   }
-  submit(){
+
+  submit(): void {
 
   }
-
 }
