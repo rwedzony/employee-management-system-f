809259c954283c26f7dec6b3a89345df67b5521e
rwedzony <rwedzony@gmail.com>
2021-09-21 16:44:22 +0200

    edit component refactor

--- src/app/content/user/editprofile/editprofile.component.ts
+++ src/app/content/user/editprofile/editprofile.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../../services/auth.service";
-import {Employeeuser} from "../../../datamodels/employeeuser";
-import {EmployeeService} from "../../../services/employee.service";
-import {Router} from "@angular/router";
-import {EdittaskdialogComponent} from "../../admin/edittaskdialog/edittaskdialog.component";
-import {EditprofiledialogComponent} from "../editprofiledialog/editprofiledialog.component";
-import {MatDialog} from "@angular/material/dialog";
-import {ToastrService} from "ngx-toastr";
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
+import {Employeeuser} from '../../../datamodels/employeeuser';
+import {EmployeeService} from '../../../services/employee.service';
+import {Router} from '@angular/router';
+import {EdittaskdialogComponent} from '../../admin/edittaskdialog/edittaskdialog.component';
+import {EditprofiledialogComponent} from '../editprofiledialog/editprofiledialog.component';
+import {MatDialog} from '@angular/material/dialog';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-editprofile',
@@ -16,8 +16,9 @@ import {ToastrService} from "ngx-toastr";
 
 export class EditprofileComponent implements OnInit {
 
-  hide=true;
+  hide = true;
   employeeUser: Employeeuser;
+
   constructor(private authService: AuthService,
               private employeeService: EmployeeService,
               private router: Router,
@@ -26,27 +27,28 @@ export class EditprofileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.employeeUser=new Employeeuser();
-    this.employeeUser.id=this.authService.getUserId();
-    this.employeeUser.firstName=this.authService.getUserFirstName();
-    this.employeeUser.lastName=this.authService.getUserLastName();
-    this.employeeUser.email=this.authService.getUserLogin();
-    this.employeeUser.password='';
+    this.employeeUser = new Employeeuser();
+    this.employeeUser.id = this.authService.getUserId();
+    this.employeeUser.firstName = this.authService.getUserFirstName();
+    this.employeeUser.lastName = this.authService.getUserLastName();
+    this.employeeUser.email = this.authService.getUserLogin();
+    this.employeeUser.password = '';
   }
 
-  submitChanges() {
-const dialogRef=this.dialog.open(EditprofiledialogComponent,{data:{employee: this.employeeUser}});
+  submitChanges(): void {
+    const dialogRef = this.dialog.open(EditprofiledialogComponent, {data: {employee: this.employeeUser}});
     dialogRef.afterClosed().subscribe(result => {
-        this.toastrService.success("You edited data and can log in again!","Success",{
-          timeOut:2000,
+        this.toastrService.success('You edited data and can log in again!', 'Success', {
+          timeOut: 2000,
         });
-      this.authService.logout();
-      this.router.navigateByUrl('');},
-      (error) => {
-      this.toastrService.error("There was some connection error","Error",{
-        timeOut:2000,
-      });
+        this.authService.logout();
+        this.router.navigateByUrl('');
       },
-      );
+      (error) => {
+        this.toastrService.error('There was some connection error', 'Error', {
+          timeOut: 2000,
+        });
+      },
+    );
   }
 }
