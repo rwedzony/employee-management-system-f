3575e8d6576f04e708a0d847347ab0634fb1e67f
rwedzony <rwedzony@gmail.com>
2021-09-22 22:22:23 +0200

    deletetask dialog refactor

--- src/app/content/admin/deletetaskdialog/deletetaskdialog.component.ts
+++ src/app/content/admin/deletetaskdialog/deletetaskdialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {TaskService} from "../../../services/task.service";
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {TaskService} from '../../../services/task.service';
 
 @Component({
   selector: 'app-deletetaskdialog',
@@ -11,20 +11,22 @@ export class DeletetaskdialogComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<DeletetaskdialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
-              private taskService: TaskService) { }
+              private taskService: TaskService) {
+  }
 
   ngOnInit(): void {
 
   }
-  onNoClick() {
+
+  onNoClick(): void {
     this.dialogRef.close();
   }
 
-  submit(){
+  submit(): void {
 
   }
 
-  confirmDelete() {
+  confirmDelete(): void {
     this.taskService.deleteTaskById(this.data.id);
 
     this.dialogRef.close();
