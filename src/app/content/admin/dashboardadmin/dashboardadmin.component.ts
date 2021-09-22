c64afb69d65750da8abaf917daa413c13b52a9e8
rwedzony <rwedzony@gmail.com>
2021-09-22 22:21:16 +0200

    dashboardadmin refactor

--- src/app/content/admin/dashboardadmin/dashboardadmin.component.ts
+++ src/app/content/admin/dashboardadmin/dashboardadmin.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from "../../../services/auth.service";
-import {TaskService} from "../../../services/task.service";
-import {EmployeeService} from "../../../services/employee.service";
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
+import {TaskService} from '../../../services/task.service';
+import {EmployeeService} from '../../../services/employee.service';
 
 @Component({
   selector: 'app-dashboardadmin',
@@ -12,26 +12,27 @@ export class DashboardadminComponent implements OnInit {
 
   constructor(public authService: AuthService,
               private taskService: TaskService,
-              private employeeService: EmployeeService) { }
+              private employeeService: EmployeeService) {
+  }
 
-  noOfAllTasks:any;
-  noOfAssingnedTasks:any;
-  noOfUnassingnedTasks:any;
-  noOfDoneTasks:any;
-  noOfNewTasks:any;
+  noOfAllTasks: any;
+  noOfAssingnedTasks: any;
+  noOfUnassingnedTasks: any;
+  noOfDoneTasks: any;
+  noOfNewTasks: any;
 
-  employeeCount :any;
+  employeeCount: any;
   employeeWagesCount: any;
 
   ngOnInit(): void {
-    this.taskService.getAllTasksCount().subscribe(data=>this.noOfAllTasks=data);
-    this.taskService.getAllATasksCount().subscribe(data=>this.noOfAssingnedTasks=data);
-    this.taskService.getAllUnATasksCount().subscribe(data=>this.noOfUnassingnedTasks=data);
-    this.taskService.getAllDoneTasksCount().subscribe(data=>this.noOfDoneTasks=data);
-    this.taskService.getAllNewTasksCount().subscribe(data=>this.noOfNewTasks=data);
+    this.taskService.getAllTasksCount().subscribe(data => this.noOfAllTasks = data);
+    this.taskService.getAllATasksCount().subscribe(data => this.noOfAssingnedTasks = data);
+    this.taskService.getAllUnATasksCount().subscribe(data => this.noOfUnassingnedTasks = data);
+    this.taskService.getAllDoneTasksCount().subscribe(data => this.noOfDoneTasks = data);
+    this.taskService.getAllNewTasksCount().subscribe(data => this.noOfNewTasks = data);
 
-    this.employeeService.getAllEmployeesCount().subscribe(data=>this.employeeCount=data);
-    this.employeeService.getAllEmployeesWagesCount().subscribe(data=>this.employeeWagesCount=data);
+    this.employeeService.getAllEmployeesCount().subscribe(data => this.employeeCount = data);
+    this.employeeService.getAllEmployeesWagesCount().subscribe(data => this.employeeWagesCount = data);
 
 
   }
