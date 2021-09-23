4ac8e9e6c8a891ca1bc5076f6ced1f246cc24652
rwedzony <rwedzony@gmail.com>
2021-09-23 20:33:28 +0200

    fixing quotemarks

--- src/app/layouts/mainlayout.module.ts
+++ src/app/layouts/mainlayout.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MainlayoutComponent} from "./mainlayout/mainlayout.component";
-import {RouterModule} from "@angular/router";
-import {FooterComponent} from "../shared/components/footer/footer.component";
-import {HeaderComponent} from "../shared/components/header/header.component";
-import {SidebarComponent} from "../shared/components/sidebar/sidebar.component";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {FlexModule} from "@angular/flex-layout";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatListModule} from "@angular/material/list";
-import {FormsModule} from "@angular/forms";
+import {MainlayoutComponent} from './mainlayout/mainlayout.component';
+import {RouterModule} from '@angular/router';
+import {FooterComponent} from '../shared/components/footer/footer.component';
+import {HeaderComponent} from '../shared/components/header/header.component';
+import {SidebarComponent} from '../shared/components/sidebar/sidebar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {FlexModule} from '@angular/flex-layout';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {FormsModule} from '@angular/forms';
 
 
 
@@ -38,7 +38,7 @@ import {FormsModule} from "@angular/forms";
     MatListModule,
     FormsModule
   ],
-  exports:[
+  exports: [
     HeaderComponent,
     FooterComponent,
     SidebarComponent
