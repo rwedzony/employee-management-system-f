e3cdeadad45e7bc10edc7a43f2856f68871baf50
rwedzony <rwedzony@gmail.com>
2021-09-24 19:12:21 +0200

    add-emp dialog refactor

--- src/app/employee_dialogs/add/add-emp-dialog.component.ts
+++ src/app/employee_dialogs/add/add-emp-dialog.component.ts
@@ -1,8 +1,8 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {EmployeeService} from "../../services/employee.service";
-import {Employee} from "../../datamodels/employee";
-import {ToastrService} from "ngx-toastr";
+import {Component, OnInit} from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+import {EmployeeService} from '../../services/employee.service';
+import {Employee} from '../../datamodels/employee';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-add',
@@ -13,49 +13,48 @@ export class AddEmpDialogComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<AddEmpDialogComponent>,
               private employeeService: EmployeeService,
-              private toastrService:ToastrService) { }
+              private toastrService: ToastrService) {
+  }
 
-  employee:Employee;
-  hide=true;
+  employee: Employee;
+  hide = true;
 
   ngOnInit(): void {
-    this.employee=new Employee();
-    this.employee.id=0;
-    this.employee.firstName='';
-    this.employee.lastName='';
-    this.employee.email='';
-    this.employee.password='';
-    this.employee.occupation='';
-    this.employee.empAuthLevel='ROLE_USER';
-    this.employee.currentMonthWorkingHours= 168;
-    this.employee.remainingDayOffs=22;
+    this.employee = new Employee();
+    this.employee.id = 0;
+    this.employee.firstName = '';
+    this.employee.lastName = '';
+    this.employee.email = '';
+    this.employee.password = '';
+    this.employee.occupation = '';
+    this.employee.empAuthLevel = 'ROLE_USER';
+    this.employee.currentMonthWorkingHours = 168;
+    this.employee.remainingDayOffs = 22;
 
 
   }
 
-  onNoClick() {
+  onNoClick(): void {
     this.dialogRef.close();
   }
 
   confirmAdd(): void {
     this.employeeService.addEmployee(this.employee).subscribe(
       (value) => {
-        this.toastrService.success("Employee Successfully Created!","Success",{
-          timeOut:2000,
+        this.toastrService.success('Employee Successfully Created!', 'Success', {
+          timeOut: 2000,
         });
       },
       (error) => {
-        this.toastrService.error("Connection Error during creating employee.","Error",{
-          timeOut:2000,
+        this.toastrService.error('Connection Error during creating employee.', 'Error', {
+          timeOut: 2000,
         });
       }
-
-
-
     );
     this.dialogRef.close();
   }
-  submit(){
+
+  submit(): void {
 
   }
 }
