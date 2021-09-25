e078950217d770bc70ccafda5644b1ad12aea4ac
rwedzony <rwedzony@gmail.com>
2021-09-25 20:17:49 +0200

    deleteemp refactor

--- src/app/employee_dialogs/delete/delete-emp-dialog.component.ts
+++ src/app/employee_dialogs/delete/delete-emp-dialog.component.ts
@@ -1,8 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import {EmployeeService} from "../../services/employee.service";
-import {Observable} from "rxjs";
-import {ToastrService} from "ngx-toastr";
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {EmployeeService} from '../../services/employee.service';
+import {ToastrService} from 'ngx-toastr';
 
 
 @Component({
@@ -15,29 +14,28 @@ export class DeleteEmpDialogComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<DeleteEmpDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any,
               private employeeService: EmployeeService,
-              private toastrService:ToastrService){ }
+              private toastrService: ToastrService) {
+  }
 
   ngOnInit(): void {
   }
 
-  onNoClick() {
+  onNoClick(): void {
     this.dialogRef.close();
   }
 
-  confirmDelete() {
+  confirmDelete(): void {
     this.employeeService.deleteEmployeesById(this.data.id).subscribe(
       (value) => {
-        this.toastrService.success("Employee Successfully Deleted!","Success",{
-          timeOut:2000,
+        this.toastrService.success('Employee Successfully Deleted!', 'Success', {
+          timeOut: 2000,
         });
       },
       (error) => {
-        this.toastrService.error("Connection Error during deleting employee.","Error",{
-          timeOut:2000,
+        this.toastrService.error('Connection Error during deleting employee.', 'Error', {
+          timeOut: 2000,
         });
       }
-
-
     );
 
     this.dialogRef.close();
